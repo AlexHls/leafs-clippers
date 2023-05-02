5d7d90ceda2d0dd0bf93ffeadc5ed14084da06c4
Alexander Holas <alex.holas@gmx.de>
2023-05-02 11:31:50 +0200

    Fixed verbose output

--- src/leafs_clippers/rewrite_tracer.py
+++ src/leafs_clippers/rewrite_tracer.py
@@ -60,7 +60,7 @@ def main(model, outfile, snappath="./", chunksize=2.5, verbose=False):
                 print("Now reading timestep %d/%d" % (itstp, ntimesteps))
 
             chunk, _ = fin.get_timestep(
-                itstp, read_count=read_count, chunksize=read_chunk, quiet=verbose
+                itstp, read_count=read_count, chunksize=read_chunk, quiet=(not verbose)
             )
             for ival in range(6):
                 data[:, itstp, ival] = chunk[ival, :]
