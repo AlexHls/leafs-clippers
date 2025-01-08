5143016201ce1aac21d2b5c192ee08fcf9511210
Alexander Holas <alex.holas@gmx.de>
2025-01-08 16:57:40 -0700

    Add cli interface

---
+++ src/leafs_clippers/cli/map_leafs_to_artis.py
@@ -0,0 +1,77 @@
+import argparse
+
+from leafs_clippers.leafs import leafs_mapping as lm
+
+
+def main(
+    snappath,
+    tppnppath,
+    one_dim=False,
+    res=100,
+    vacuum_threshold=1e-4,
+    max_vel=0.0,
+):
+    map = lm.LeafsMapping(snappath=snappath, tppnppath=tppnppath)
+
+    if one_dim:
+        map.map1D(resolution=res, vacuum_threshold=vacuum_threshold, max_vel=max_vel)
+    else:
+        map.map3D(resolution=res, vacuum_threshold=vacuum_threshold, max_vel=max_vel)
+
+    return
+
+
+def cli():
+    parser = argparse.ArgumentParser(description="Map leafs simulations to ARTIS")
+
+    parser.add_argument(
+        "snappath",
+        type=str,
+        help="Path to the snapshot directory",
+    )
+    parser.add_argument(
+        "tppnppath",
+        type=str,
+        help="Path to the TPPNP output directory",
+    )
+    parser.add_argument(
+        "--one_dim",
+        action="store_true",
+        help="Map to 1D, else 3D",
+    )
+    parser.add_argument(
+        "-r",
+        "--resolution",
+        type=int,
+        default=100,
+        help="Resolution of the mapped output",
+    )
+    parser.add_argument(
+        "--vacuum_threshold",
+        type=float,
+        default=1e-4,
+        help="Threshold for vacuum",
+    )
+    parser.add_argument(
+        "--max_vel",
+        type=float,
+        default=0.0,
+        help="Maximum velocity for the mapping",
+    )
+
+    args = parser.parse_args()
+
+    main(
+        args.snappath,
+        args.tppnppath,
+        one_dim=args.one_dim,
+        res=args.resolution,
+        vacuum_threshold=args.vacuum_threshold,
+        max_vel=args.max_vel,
+    )
+
+    return
+
+
+if __name__ == "__main__":
+    cli()
