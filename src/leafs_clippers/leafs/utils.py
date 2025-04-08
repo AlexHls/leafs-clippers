702206a5268390b9163b854ccebc0e04cb175420
Alexander Holas <alex.holas@gmx.de>
2025-04-08 23:15:07 +0200

    Version that sorta works for 3D

--- src/leafs_clippers/leafs/utils.py
+++ src/leafs_clippers/leafs/utils.py
@@ -40,6 +40,9 @@ class LeafsXdmf3Writer:
         self.attribute_shape = (snapshot.gnx, snapshot.gny, snapshot.gnz)
         self.outname = snapshot.basename + ".xdmf"
         self.subgrid_size = subgrid_size or self.grid_shape
+        # In case of 2D grid, set the z dimension to 1
+        if snapshot.gnz == 1:
+            self.subgrid_size = (self.subgrid_size[0], self.subgrid_size[1], 1)
 
     @property
     def _ignore_keys(self):
@@ -74,10 +77,13 @@ class LeafsXdmf3Writer:
         for x in range(0, nx - 1, sx):
             for y in range(0, ny - 1, sy):
                 for z in range(0, nz - 1, sz):
-                    yield (x, y, z), (
-                        min(sx + 1, nx - x),
-                        min(sy + 1, ny - y),
-                        min(sz + 1, nz - z),
+                    yield (
+                        (x, y, z),
+                        (
+                            min(sx + 1, nx - x),
+                            min(sy + 1, ny - y),
+                            min(sz + 1, nz - z),
+                        ),
                     )
 
     def _write_header(self, f):
@@ -85,7 +91,9 @@ class LeafsXdmf3Writer:
         f.write('<Xdmf Version="3.0">\n')
         f.write("<Domain>\n")
         # Add CollectionType="Spatial" to the Grid element
-        f.write('<Grid Name="3DStructuredGrid" GridType="Collection" CollectionType="Spatial">\n')
+        f.write(
+            '<Grid Name="3DStructuredGrid" GridType="Collection" CollectionType="Spatial">\n'
+        )
 
     def _write_footer(self, f):
         f.write("</Grid>\n")
@@ -98,30 +106,42 @@ class LeafsXdmf3Writer:
     def _write_topology(self, f, grid_shape: tuple, offset: tuple) -> None:
         f.write(
             '<Topology TopologyType="3DRectMesh" Dimensions="{} {} {}">\n'.format(
-                grid_shape[0], grid_shape[1], grid_shape[2]
+                grid_shape[2], grid_shape[1], grid_shape[0]
             )
         )
         f.write("</Topology>\n")
 
-    def _write_geometry(self, f, grid_shape: tuple, offset: tuple, filename: str) -> None:
+    def _write_geometry(
+        self, f, grid_shape: tuple, offset: tuple, filename: str
+    ) -> None:
         f.write('<Geometry GeometryType="VXVYVZ">\n')
         for dim, edge, start in zip(grid_shape, ["edgex", "edgey", "edgez"], offset):
             f.write('<DataItem ItemType="HyperSlab" Dimensions="{}">\n'.format(dim))
             f.write('<DataItem Dimensions="3 1" NumberType="Int" Format="XML">\n')
             f.write(f"{start} 1 {dim}\n")  # Start, Stride, Count
             f.write("</DataItem>\n")
-            f.write('<DataItem Dimensions="{}" NumberType="Float" Precision="8" Format="HDF">\n'.format(dim))
+            f.write(
+                '<DataItem Dimensions="{}" NumberType="Float" Precision="8" Format="HDF">\n'.format(
+                    dim
+                )
+            )
             f.write(f"{filename}:/{edge}\n")
             f.write("</DataItem>\n")
             f.write("</DataItem>\n")
         f.write("</Geometry>\n")
 
-    def _write_attribute(self, f, label: str, grid_shape: tuple, offset: tuple, filename: str) -> None:
+    def _write_attribute(
+        self, f, label: str, grid_shape: tuple, offset: tuple, filename: str
+    ) -> None:
         f.write(f'<Attribute Name="{label}" AttributeType="Scalar" Center="Cell">\n')
-        f.write('<DataItem ItemType="HyperSlab" Dimensions="{} {} {}">\n'.format(grid_shape[0]-1, grid_shape[1]-1, grid_shape[2]-1))
+        f.write(
+            '<DataItem ItemType="HyperSlab" Dimensions="{} {} {}">\n'.format(
+                grid_shape[2] - 1, grid_shape[1] - 1, grid_shape[0] - 1
+            )
+        )
         f.write('<DataItem Dimensions="3 3" NumberType="Int" Format="XML">\n')
         f.write(
-            f"{offset[0]} {offset[1]} {offset[2]} 1 1 1 {grid_shape[0]-1} {grid_shape[1]-1} {grid_shape[2]-1}\n"
+            f"{offset[2]} {offset[1]} {offset[0]}\n1 1 1\n{grid_shape[2] - 1} {grid_shape[1] - 1} {grid_shape[0] - 1}\n"
         )  # Start, Stride, Count
         f.write("</DataItem>\n")
         f.write(
@@ -149,7 +169,9 @@ class LeafsXdmf3Writer:
                 for label in self.snapshot.keys:
                     if label in self._ignore_keys:
                         continue
-                    self._write_attribute(f, label, subgrid_shape, offset, self.filename)
+                    self._write_attribute(
+                        f, label, subgrid_shape, offset, self.filename
+                    )
                 f.write("</Grid>\n")
             self._write_footer(f)
         return outname
